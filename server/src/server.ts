6b8fa8a195d2a7615a06f61e5637edfde7035ab7
NalbertC <nalberthcastro1510@gmail.com>
2023-11-22 19:34:00 -0300

    servidor funcionando

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+dotenv.config();
+
+import { routes } from "./routes";
+
+const server = express();
+
+// middleware
+server.use(cors());
+server.use(morgan("dev"));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(
+    `Server running in ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
+  );
+});
